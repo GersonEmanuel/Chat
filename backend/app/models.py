b37a12549ca9ccfe3708dc9a716932a637d7ac99
GersonEmanuel <gersonemanuel432@gmail.com>
2025-04-08 22:59:55 -0300

    alembic for models changes

---
+++ backend/app/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Table, Column, Integer, String, DateTime, MetaData, ForeignKey
+import datetime
+
+metadata_obj = MetaData()
+
+users = Table(
+    "users",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("username", String, unique=True)
+)
+
+messages = Table(
+    "messages",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("content", String),
+    Column("timestamp", DateTime, default=datetime.datetime.now()),
+    Column("user_id", Integer, ForeignKey("users.id"))
+    
+)
